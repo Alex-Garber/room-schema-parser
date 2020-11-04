68e9beb602b9d32195a8295a8d819cf1393eebc5
Alex Garber <alexgarber6@gmail.com>
2020-11-04 11:52:56 -0700

    Addded support for View ddl

--- src/edu/cnm/deepdive/model/Database.java
+++ src/edu/cnm/deepdive/model/Database.java
@@ -4,8 +4,12 @@ import com.google.gson.annotations.Expose;
 import java.util.List;
 
 public class Database {
-@Expose
- private List<Entity> entities;
+
+  @Expose
+  private List<Entity> entities;
+
+  @Expose
+  private List<View> views;
 
   public List<Entity> getEntities() {
     return entities;
@@ -13,5 +17,13 @@ public class Database {
 
   public void setEntities(List<Entity> entities) {
     this.entities = entities;
+  }
+
+  public List<View> getViews() {
+    return views;
+  }
+
+  public void setViews(List<View> views) {
+    this.views = views;
   }
 }
